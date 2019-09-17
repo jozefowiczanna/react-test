0355211c349047cc843d516789d459099023c56e
Anna Józefowicz <anna.jozefowicz.86@gmail.com>
2019-09-17 06:24:54 +0200

    Updates

---
+++ precache-manifest.81426f70ee069395afabc8621e0e7535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d466dad22705e1de6227f7649876755",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "c5be6200c828638e8988",
+    "url": "/react-test/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f97b971bedba0e0eda08",
+    "url": "/react-test/static/js/2.a741f7b3.chunk.js"
+  },
+  {
+    "revision": "c5be6200c828638e8988",
+    "url": "/react-test/static/js/main.7680b08a.chunk.js"
+  },
+  {
+    "revision": "aaa1ada1743192a6c8c1",
+    "url": "/react-test/static/js/runtime~main.e995f18d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
